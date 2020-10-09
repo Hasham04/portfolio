3933b43b6e6ac4b42afba368bb4f151a0b62e34c
Hasham Rasheed <69496736+Hasham04@users.noreply.github.com>
2020-10-08 20:34:11 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,17 +2,46 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox
 
 workbox.routing.registerRoute(
   // Cache style resources, i.e. CSS files.
-  /\.(?:css|js)$/,
+  ({request}) => request.destination === 'script' ||
+  request.destination === 'style',
   // Use cache but update in the background.
   new workbox.strategies.StaleWhileRevalidate({
     // Use a custom cache name.
-    "cacheName": 'css-cache',
+    "cacheName": 'static-resources',
   })
 );
 
 workbox.routing.registerRoute(
+  ({url}) => url.origin === 'https://fonts.googleapis.com',
+  new workbox.strategies.StaleWhileRevalidate({
+    // Use a custom cache name.
+    "cacheName": 'google-fonts-stylesheets',
+  })
+);
+
+workbox.routing.registerRoute(
+  ({url}) => url.origin === 'https://fonts.gstatic.com',
+
+    new workbox.strategies.StaleWhileRevalidate({
+    // Use a custom cache name.
+    "cacheName": 'google-fonts-webfonts',
+    plugins: [
+      new workbox.cacheableResponse.CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new workbox.expiration.ExpirationPlugin({
+        // Cache for a maximum of a week.
+        maxAgeSeconds: 60 * 60 * 24 * 365,
+        maxEntries: 30,
+      }),
+    ],
+  }),
+);
+
+workbox.routing.registerRoute(
   // Cache image files.
-  /\.(?:png|jpg|mp4)$/,
+  ({request}) => request.destination === 'video' ||
+                  request.destination === 'image',
   // Use the cache if it's available.
   new workbox.strategies.CacheFirst({
     // Use a custom cache name.
@@ -20,7 +49,7 @@ workbox.routing.registerRoute(
     plugins: [
       new workbox.expiration.ExpirationPlugin({
         // Cache only 20 images.
-        maxEntries: 50,
+        maxEntries: 60,
         // Cache for a maximum of a week.
         maxAgeSeconds: 7 * 24 * 60 * 60,
       })
